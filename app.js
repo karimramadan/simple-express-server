55430350cb38e78bf81eb8193b44d4e087a8618d
Karim ramadan <karim.ramadan@gmail.com>
2019-09-29 05:45:13 +0700

    create all methods: add, put, and delete

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
 const express = require("express");
 const cors = require("cors");
-const port = process.env.PORT || 3000;
+var fs = require('fs');
+const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
+app.use(express.json()) 
 
-const contacts = require('./data');
+const db = "./data.json";
+const contacts = require(db);
 
 // Endpoints
 app.get('/contacts', function (req, res) {
     res.json(contacts)
+})
+// Methods
+app.post('/contacts', function (req, res) {
+    let newContact = req.body;
+    contacts.contacts.push( newContact );
+    fs.writeFileSync(db, JSON.stringify(contacts, null, 2));
+    res.json('Contact has been added successfully');
+})
+app.put('/contacts/:id', function (req, res) {
+    let updatedContact = req.body;
+    let foundIndex = contacts.contacts.findIndex(contact => contact.id == req.params.id);
+    contacts.contacts[foundIndex] = updatedContact;
+    fs.writeFileSync(db, JSON.stringify(contacts, null, 2));
+    res.json('Contact has been updated successfully');
+})
+app.delete('/contacts/:id', function (req, res) {
+    let filteredContacts = contacts.contacts.filter((contact) => contact.id != req.params.id);
+    contacts.contacts = filteredContacts;
+    fs.writeFileSync(db, JSON.stringify(contacts, null, 2));
+    res.json('Contact has been deleted successfully');
 })
 
 app.listen(port, () => {
