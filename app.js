ac9d1ee2dd2adaa6d4d24583524812e9d0ee4f9f
Karim ramadan <karim.ramadan@gmail.com>
2019-10-08 00:22:05 +0700

    update to work with firebase auth

--- app.js
+++ app.js
@@ -7,29 +7,56 @@ const app = express();
 app.use(cors());
 app.use(express.json()) 
 
-const db = "./data.json";
-const contacts = require(db);
+// const db = "./data.json";
+// const contacts = require(db);
 
 // Endpoints
 app.get('/contacts', function (req, res) {
+    const db = `./${req.query.uid}.json`;
+    const contacts = require(db);
     res.json(contacts)
+    // console.log(req.query.uid);
 })
+// Signout
+app.post('/sessionLogout', (req, res) => {
+    res.clearCookie('session');
+});
 // Methods
 app.post('/contacts', function (req, res) {
+    const db = `./${req.query.uid}.json`;
+    const contacts = require(db);
     let newContact = req.body;
     contacts.contacts.push( newContact );
     fs.writeFileSync(db, JSON.stringify(contacts, null, 2));
     res.json('Contact has been added successfully');
 })
-app.put('/contacts/:id', function (req, res) {
+app.post('/:uid', function (req, res) {
+    const userFile = `${req.params.uid}.json`;
+    const blueprint = '{"contacts": []}';
+    const jsonObj = JSON.parse(blueprint);
+    const jsonContent = JSON.stringify(jsonObj);
+    fs.writeFile(userFile, jsonContent, 'utf8', (err) => {
+        if (err) {
+            console.log("An error occured while writing JSON Object to File.");
+            return console.log(err);
+        }
+        console.log('New file created')
+    })
+    res.json('File has been created successfully');
+})
+app.put('/contacts', function (req, res) {
+    const db = `./${req.query.uid}.json`;
+    const contacts = require(db);
     let updatedContact = req.body;
-    let foundIndex = contacts.contacts.findIndex(contact => contact.id == req.params.id);
+    let foundIndex = contacts.contacts.findIndex(contact => contact.id == req.query.id);
     contacts.contacts[foundIndex] = updatedContact;
     fs.writeFileSync(db, JSON.stringify(contacts, null, 2));
     res.json('Contact has been updated successfully');
 })
-app.delete('/contacts/:id', function (req, res) {
-    let filteredContacts = contacts.contacts.filter((contact) => contact.id != req.params.id);
+app.delete('/contacts', function (req, res) {
+    const db = `./${req.query.uid}.json`;
+    const contacts = require(db);
+    let filteredContacts = contacts.contacts.filter((contact) => contact.id != req.query.id);
     contacts.contacts = filteredContacts;
     fs.writeFileSync(db, JSON.stringify(contacts, null, 2));
     res.json('Contact has been deleted successfully');
